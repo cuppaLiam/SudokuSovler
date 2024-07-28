c33bd10c07b574be512bf4a07bd9c6d06be9f617
ImCloudy <Liamcoffey24@gmail.com>
2024-07-28 20:56:31 +1000

    working obvPairs and hidPairs functions; should be at least. primary purpose of pointing function done, further features need to be added

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int sudokuInput[9][9] =  {{0,0,3,0,0,0,7,0,0},
-                          {4,8,9,7,3,0,0,0,0},
-                          {0,0,6,0,9,0,8,0,0},
-                          {5,9,0,0,0,1,0,0,0},
-                          {0,0,0,0,5,0,3,0,0},
-                          {3,0,0,0,4,0,0,1,5},
-                          {0,0,0,6,0,5,0,0,2},
-                          {0,0,4,0,0,0,0,0,0},
-                          {2,0,0,0,0,0,1,0,3}};
+
+int sudokuInput[9][9] =  {{0,0,3,7,2,0,1,0,6},
+                          {0,0,0,0,0,0,5,3,0},
+                          {1,0,0,0,0,0,7,0,0},
+                          {7,0,8,6,0,0,0,0,0},
+                          {4,0,0,0,0,0,0,0,1},
+                          {0,6,0,0,0,8,0,0,0},
+                          {0,0,5,0,8,4,3,0,0},
+                          {0,0,0,0,1,0,0,8,0},
+                          {0,0,0,0,0,5,0,0,2}};
 
 typedef struct Cell {
     int value;
@@ -171,6 +172,113 @@ void displayPossibleValues(Cell sudoku[9][9]){
     }
 }
 
+bool obvPairs(Cell unitCells[9]) {
+    bool updated = false;
+    int occurrences[9] = {0};
+
+    // Count occurrences of each possible value in the unit
+    for (int count = 0; count < 9; count++) {
+        Cell* current_cell = &unitCells[count];
+        for (int posVal = 0; posVal < 9; posVal++) {
+            if (current_cell->possibleValues[posVal]) {
+                occurrences[posVal]++;
+            }
+        }
+    }
+
+    // Identify and process pairs and triplets
+    for (int posVal = 0; posVal < 9; posVal++) {
+        if (occurrences[posVal] == 2 || occurrences[posVal] == 3) {
+            Cell* cells[3] = {NULL, NULL, NULL};
+            int cellCount = 0;
+
+            // Collect cells with the current possible value
+            for (int count = 0; count < 9; count++) {
+                if (unitCells[count].possibleValues[posVal]) {
+                    cells[cellCount++] = &unitCells[count];
+                }
+            }
+
+            // Check if all collected cells have the same possible values
+            bool samePossibilities = true;
+            for (int i = 1; i < cellCount; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (cells[0]->possibleValues[j] != cells[i]->possibleValues[j]) {
+                        samePossibilities = false;
+                        break;
+                    }
+                }
+                if (!samePossibilities) break;
+            }
+
+            if (samePossibilities) {
+                // Eliminate these values from other cells in the unit
+                for (int count = 0; count < 9; count++) {
+                    if (&unitCells[count] != cells[0] && &unitCells[count] != cells[1] && (cellCount == 2 || &unitCells[count] != cells[2])) {
+                        if (unitCells[count].possibleValues[posVal]) {
+                            unitCells[count].possibleValues[posVal] = false;
+                            updated = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return updated;
+}
+
+bool hidPairs(Cell unitCells[9]) {
+    bool updated = false;
+
+    for (int first = 0; first < 8; first++) {
+        for (int second = first + 1; second < 9; second++) {
+            int count = 0;
+            Cell *pairCells[2] = {NULL, NULL};
+            int pairCount = 0;
+
+            // Find cells where only 'first' and 'second' are possible
+            for (int cellIndex = 0; cellIndex < 9; cellIndex++) {
+                Cell *cell = &unitCells[cellIndex];
+                if (cell->possibleValues[first] && cell->possibleValues[second]) {
+                    // Check that these are the only possibilities for this cell
+                    bool validPair = true;
+                    for (int posVal = 0; posVal < 9; posVal++) {
+                        if (posVal != first && posVal != second && cell->possibleValues[posVal]) {
+                            validPair = false;
+                            break;
+                        }
+                    }
+                    if (validPair) {
+                        if (pairCount < 2) {
+                            pairCells[pairCount++] = cell;
+                        }
+                        count++;
+                    }
+                }
+            }
+
+            if (count == 2 && pairCount == 2) {
+                // We found exactly two cells with this hidden pair, clear other values
+                for (int posVal = 0; posVal < 9; posVal++) {
+                    if (posVal != first && posVal != second) {
+                        if (pairCells[0]->possibleValues[posVal] || pairCells[1]->possibleValues[posVal]) {
+                            pairCells[0]->possibleValues[posVal] = false;
+                            pairCells[1]->possibleValues[posVal] = false;
+                            updated = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return updated;
+}
+
+
+
+
 bool pointing(Cell sudoku[9][9]) {
     printf("\npointing function called");
     bool updated = false;
@@ -227,7 +335,7 @@ bool pointing(Cell sudoku[9][9]) {
     return updated;
 }
 
-void solve(Cell sudoku[9][9]){
+void solve(Cell sudoku[9][9]) {
     bool update_made;
     int itr = 0;
 
@@ -259,17 +367,39 @@ void solve(Cell sudoku[9][9]){
             }
         } while (cell_solved);
 
-        // Process pointing function
+        // Process obvious pairs/triplets
+        for (int i = 0; i < 9; i++) {
+            if (obvPairs(sudoku[i])) update_made = true;      // Row
+            if (obvPairs(sudoku[i][0].colCells)) update_made = true; // Column
+            if (obvPairs(sudoku[i][0].regCells)) update_made = true; // Region
+        }
 
+        // Process hidden pairs/triplets
+        for (int i = 0; i < 9; i++) {
+            if (hidPairs(sudoku[i])) update_made = true;      // Row
+            if (hidPairs(sudoku[i][0].colCells)) update_made = true; // Column
+            if (hidPairs(sudoku[i][0].regCells)) update_made = true; // Region
+        }
+
+
+        //?
+        if (!update_made) {
+            break;
+        }
+
+
+        // Process pointing function
         bool pointing_updated = pointing(sudoku);
         if (pointing_updated) {
             update_made = true;
         }
+        
 
         printf("\nUpdate made: %s", update_made ? "true" : "false");
 
     } while (update_made);
 }
+
 
 
 int main(){
